2a34f57708dbf65bad7105dfe9ec66a50f744eff
Ohtaki-endless <orora_togiants@yahoo.co.jp>
2022-06-25 16:04:52 +0900

    edit PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        // $posts = $post->getByLimit();
+        // dd($posts);
+        return view('posts/index')->with(['posts' => $post->getByLimit()]);
     }
 }
+?>
